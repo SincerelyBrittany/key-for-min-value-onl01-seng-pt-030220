5d8bdaca187abd9db60bdd19a371e4ad57b3498b
Brittany Russell <sincerelybr@gmail.com>
2020-03-11 22:30:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   cheapest_item = nil 
   cheapest_price = nil
-  name_hash.each do |name, key|
-    if cheapest_price == nil || key < cheapest_price
-       cheapest_price = key
+  name_hash.each do |name,  value
+    if cheapest_price == nil ||  value< cheapest_price
+       cheapest_price = value
        cheapest_item = name
     end 
   end
