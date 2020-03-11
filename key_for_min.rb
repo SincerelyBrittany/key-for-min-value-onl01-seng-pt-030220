fd9ecb2ebbe379f9e78344daa9f81c479501b6a9
Brittany Russell <sincerelybr@gmail.com>
2020-03-11 22:26:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  cheapest_item = nil 
+  cheapest_price = nil
   name_hash.each do |name, key|
   if key > key
     key 
